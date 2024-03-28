2743f2dd464f5c8d3a5a08056855d03988fccd75
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-28 14:04:28 +0100

    Change debug to true

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -81,8 +81,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEV' in os.environ
-
-DEBUG = False  # Set DEBUG to False for production
+if 'DEV' in os.environ:
+    DEBUG = True  # Set DEBUG to True for development
+else:
+    DEBUG = False  # Set DEBUG to False for production
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'world-of-craft-670e0fb14b24.herokuapp.com']
 """
