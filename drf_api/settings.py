5dfac16c1d6cb40026f91d90db27da44037d59c6
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-28 14:34:29 +0100

    Change allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -83,7 +83,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = 'DEV' in os.environ
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'world-of-craft-670e0fb14b24.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-etidemo-cipp5backend-luf7m25822q.ws-eu110.gitpod.io', 'world-of-craft-670e0fb14b24.herokuapp.com', ]
 """
 CORS_ALLOWED_ORIGIN_REGEXES = []
 
