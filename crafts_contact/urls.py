d780360a3015b560ace5c101219d3768bb0d260e
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-28 11:05:37 +0100

    Change crafts_contacts

--- crafts_contact/urls.py
+++ crafts_contact/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import ContactAPIView, SubmitContactFormView
+from .views import ContactAPIView, contact_us_view
 
-urlpatterns = [
-    path('submit_contact_form/', SubmitContactFormView.as_view(), name='submit_contact_form'),
-]
 
-"""
 urlpatterns = [
     path('contact-us/', contact_us_view, name='contact_us'),  # For rendering HTML
     path('api/contact-us/', ContactAPIView.as_view(), name='contact_us_api'),  # For handling API requests
 ]
-"""
