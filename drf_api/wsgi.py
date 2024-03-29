0819fe44b848a0c55ed56e04de0c3642520e0f9f
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-29 15:24:45 +0100

    Remove Whitenoise from middleware and wsgi.py

--- drf_api/wsgi.py
+++ drf_api/wsgi.py
@@ -8,10 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf_api.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root="/Users/elvir/world_of_craft/world_of_craft/backend/staticfiles")
