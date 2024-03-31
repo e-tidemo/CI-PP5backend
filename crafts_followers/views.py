e0aa2f9df3eea4964122a406a5dc3dbc25d97e76
Elvira Tidemo <elvir@MacBook-Air.local>
2024-03-31 17:26:49 +0200

    Remove trailing commas causing TypeError

--- crafts_followers/views.py
+++ crafts_followers/views.py
@@ -4,14 +4,14 @@ from crafts_followers.models import Followers
 from crafts_followers.serializers import FollowerSerializer
 
 class FollowersList(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly],
-    serializer_class = FollowerSerializer,
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = FollowerSerializer
     queryset = Followers.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 class FollowersDetail(generics.RetrieveDestroyAPIView):
-    permission_classes = [IsOwnerOrReadOnly],
-    serializer_class = FollowerSerializer, 
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
     queryset = Followers.objects.all()
