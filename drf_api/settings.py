137b9ddd40b08c6c8179a1c130a45c8c23cfa727
Elvira Tidemo <elvir@MacBook-Air.local>
2024-04-01 16:11:00 +0200

    Add 'http://localhost:3000' to allowed hosts

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -86,7 +86,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # DEBUG = 'DEV' in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '8000-etidemo-cipp5backend-luf7m25822q.ws-eu110.gitpod.io', 'world-of-craft-670e0fb14b24.herokuapp.com', ]
+ALLOWED_HOSTS = ['http://localhost:3000', '127.0.0.1', '8000-etidemo-cipp5backend-luf7m25822q.ws-eu110.gitpod.io', 'world-of-craft-670e0fb14b24.herokuapp.com', ]
 """
 CORS_ALLOWED_ORIGIN_REGEXES = []
 
