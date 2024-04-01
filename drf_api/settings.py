a0d55cfe7ba72a3264527ac649e8cd6fd31f77ca
Elvira Tidemo <elvir@MacBook-Air.local>
2024-04-01 16:15:47 +0200

    Add CSRF Trusted origins

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -87,6 +87,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['http://localhost:3000', '127.0.0.1', '8000-etidemo-cipp5backend-luf7m25822q.ws-eu110.gitpod.io', 'world-of-craft-670e0fb14b24.herokuapp.com', ]
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'https://world-of-craft-frontend-f2bcd7b16534.herokuapp.com', ]
+
 """
 CORS_ALLOWED_ORIGIN_REGEXES = []
 
