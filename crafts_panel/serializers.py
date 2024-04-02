954c9b5a4841699d145c6d39ac64e928d78823d6
Elvira Tidemo <elvir@MacBook-Air.local>
2024-04-02 08:54:11 +0200

    Reformat the crafts_panel to more like blog post

--- crafts_panel/serializers.py
+++ crafts_panel/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
-from rest_framework.exceptions import PermissionDenied
 from .models import Panel
 
 class PanelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Panel
-        fields = ['id', 'owner', 'created_at', 'updated_at', 'title', 'content', 'image', 'video_url']
-        read_only_fields = ['id', 'created_at', 'updated_at']
-
-    def create(self, validated_data):
-        # Set the owner of the post to the current user making the request
-        validated_data['owner'] = self.context['request'].user
-        return super().create(validated_data)
+        fields = ['id', 'owner', 'title', 'content', 'image', 'alt_text', 'video_url', 'created_at', 'published']
--- crafts_panel/views.py
+++ crafts_panel/views.py
@@ -1,31 +1,51 @@
-from rest_framework import generics, status
-from django.views.generic import DetailView
-from django.http import JsonResponse
-from rest_framework.exceptions import PermissionDenied
-from rest_framework.permissions import IsAdminUser
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Panel
 from .serializers import PanelSerializer
-from django.shortcuts import render
 
-class PanelCreateView(generics.CreateAPIView):
-    queryset = Panel.objects.all()
-    serializer_class = PanelSerializer
-    permission_classes = [IsAdminUser]
+@api_view(['GET'])
+def panel_list(request):
+    panels = Panel.objects.filter(published=True)
+    serializer = PanelSerializer(panels, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def panel_detail(request, pk):
+    panel = get_object_or_404(Panel, pk=pk)
+    serializer = PanelSerializer(panel)
+    return Response(serializer.data)
 
 
-    def perform_create(self, serializer):
-        if not self.request.user.is_staff:
-            raise PermissionDenied("You do not have permission to create a panel post.")
-        serializer.save()
 
-def panel_detail(request, panel_id):
-    panel = Panel.objects.get(pk=panel_id)
-    return render(request, 'panel_detail.html', {'panel': panel})
+"""    from rest_framework import generics, status
+    from django.views.generic import DetailView
+    from django.http import JsonResponse
+    from rest_framework.exceptions import PermissionDenied
+    from rest_framework.permissions import IsAdminUser
+    from .models import Panel
+    from .serializers import PanelSerializer
+    from django.shortcuts import render
 
-def panel_detail_api(request, panel_id):
-    panel = Panel.objects.get(pk=panel_id)
-    return JsonResponse({
-        'title': panel.title,
-        'image': panel.image.url if panel.image else None,
-        'content': panel.content,
-    })
+    class PanelCreateView(generics.CreateAPIView):
+        queryset = Panel.objects.all()
+        serializer_class = PanelSerializer
+        permission_classes = [IsAdminUser]
+
+
+        def perform_create(self, serializer):
+            if not self.request.user.is_staff:
+                raise PermissionDenied("You do not have permission to create a panel post.")
+            serializer.save()
+
+    def panel_detail(request, panel_id):
+        panel = Panel.objects.get(pk=panel_id)
+        return render(request, 'panel_detail.html', {'panel': panel})
+
+    def panel_detail_api(request, panel_id):
+        panel = Panel.objects.get(pk=panel_id)
+        return JsonResponse({
+            'title': panel.title,
+            'image': panel.image.url if panel.image else None,
+            'content': panel.content,
+        }) """
